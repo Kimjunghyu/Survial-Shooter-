5768a2602d151a33a931bd85697b336e4a06da5e
김정현 <wjdgus04175@gmail.com>
2024-04-25 15:39:14 +0900

    Player Animation, Move, Rotation 추가

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -1,18 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   // public string fireButtonName = "Fire1";
 
-    // Update is called once per frame
-    void Update()
+    public float speed = 5f;
+    private Animator animator;
+
+    private void Start()
     {
-        
+        animator = GetComponent<Animator>();
+
+    }
+    private void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayDistance;
+
+        if (groundPlane.Raycast(ray, out rayDistance))
+        {
+            Vector3 point = ray.GetPoint(rayDistance);
+            Vector3 direction = point - transform.position;
+            direction.y = 0;
+            Quaternion lookRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 10f);
+        }
+
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+        animator.SetFloat("Blend", Math.Abs(moveX) + Math.Abs(moveZ));
+        //bool fire = Input.GetButton(fireButtonName);
+        Vector3 pos = new Vector3(moveX, 0f, moveZ);
+        pos.Normalize();
+        transform.position += pos * speed * Time.deltaTime;
+
     }
 }
